65bfd03631d9c6cb8ab62103a7af7f86cb6f677b
Lucius87 <lion1987@gmail.com>
2021-08-09 15:31:30 +0300

    Just start

---
+++ components/section1/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>index</h1>
+</template>
+
+<script>
+export default {
+  name: "Section1",
+  data: () => ({}),
+  methods: {},
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
